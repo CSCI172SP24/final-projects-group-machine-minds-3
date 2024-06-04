b77e9d68d1741b499e3832867bfad95d7c3659a0
jonah-duncan <jonahdunc@gmail.com>
2024-06-04 15:37:17 -0700

    Add files via upload

--- mm_group_project/mm_group_project.ino
+++ mm_group_project/mm_group_project.ino
@@ -9,47 +9,56 @@
 
 /* Libraries */
 #include <IRremote.h>
-#include <Servo.h>
 
-#define ECHO_PIN     4      // Ultrasonic sensor echo pin     -> pin D4
-#define TRIG_PIN     5      // Ultrasonic sensor trigger pin  -> pin D5
-#define IR_GRN_PIN   2      // IR sensor pair green wire      -> pin D2
-#define IR_YLW_PIN   3      // IR sensor pair yellow wire     -> pin D3
+/* IR Receiver Pins/Logic */
 #define IR_RECV_PIN  10     // IR receiver Signal pin connect -> pin D10
 IRrecv irrecv(IR_RECV_PIN); // Create an instance of the IRrecv class with the specified pin
 decode_results IRresults;
 
 /* Movement Pins */
-#define RIGHT_SPD_PIN    9   // Right PWM pin               -> MODEL-X ENA
-#define RIGHT_DIR_PIN_1  12  // Right Motor direction pin 1 -> MODEL-X IN1 
-#define RIGHT_DIR_PIN_2  11  // Right Motor direction pin 2 -> MODEL-X IN2
-#define LEFT_SPD_PIN     6   // Left PWM pin                -> MODEL-X ENB
-#define LEFT_DIR_PIN_1   7   // Left Motor direction pin 1  -> MODEL-X IN3 
-#define LEFT_DIR_PIN_2   8   // Left Motor direction pin 1  -> MODEL-X IN4 
+#define RIGHT_SPD_PIN    9  // Right PWM pin               -> MODEL-X ENA
+#define RIGHT_DIR_PIN_1  12 // Right Motor direction pin 1 -> MODEL-X IN1 
+#define RIGHT_DIR_PIN_2  11 // Right Motor direction pin 2 -> MODEL-X IN2
+#define LEFT_SPD_PIN     6  // Left PWM pin                -> MODEL-X ENB
+#define LEFT_DIR_PIN_1   7  // Left Motor direction pin 1  -> MODEL-X IN3
+#define LEFT_DIR_PIN_2   8  // Left Motor direction pin 1  -> MODEL-X IN4
 
-/* IR Buttons */
-#define IR_BTN_UP     0x00FF18E7  // Code from IR controller "▲" button
-#define IR_BTN_DOWN   0x00FF4AB5  // Code from IR controller "▼" button
-#define IR_BTN_RIGHT  0x00FF5AA5  // Code from IR controller ">" button
-#define IR_BTN_LEFT   0x00FF10EF  // Code from IR controller "<" button
-#define IR_BTN_OK     0x00FF38C7  // Code from IR controller "OK" button
-#define IR_BTN_HASH   0x00FFB04F  // Code from IR controller "#" button
-#define IR_BTN_STAR   0x00FF6897  // Code from IR controller "*" button
-#define IR_BTN_1	  0x00FFA25D  // Code from IR controller "1" button
-#define IR_BTN_2	  0x00FF629D  // Code from IR controller "2" button
-#define IR_BTN_3	  0x00FFE21D  // Code from IR controller "3" button
-#define IR_BTN_4	  0x00FF22DD  // Code from IR controller "4" button
-#define IR_BTN_5	  0x00FF02FD  // Code from IR controller "5" button
-#define IR_BTN_6	  0x00FFC23D  // Code from IR controller "6" button
-#define IR_BTN_7	  0x00FFE01F  // Code from IR controller "7" button
-#define IR_BTN_8	  0x00FFA857  // Code from IR controller "8" button
-#define IR_BTN_9	  0x00FF906F  // Code from IR controller "9" button
-#define IR_BTN_0	  0x00FF9867  // Code from IR controller "10" button
+/* IR Button Pins */
+#define IR_BTN_UP     0x00FF18E7 // "▲" button
+#define IR_BTN_DOWN   0x00FF4AB5 // "▼" button
+#define IR_BTN_RIGHT  0x00FF5AA5 // ">" button
+#define IR_BTN_LEFT   0x00FF10EF // "<" button
+#define IR_BTN_OK     0x00FF38C7 // "OK" button
+#define IR_BTN_HASH   0x00FFB04F // "#" button
+#define IR_BTN_STAR   0x00FF6897 // "*" button
+#define IR_BTN_1	    0x00FFA25D // "1" button
+#define IR_BTN_2	    0x00FF629D // "2" button
+#define IR_BTN_3	    0x00FFE21D // "3" button
+#define IR_BTN_4	    0x00FF22DD // "4" button
+#define IR_BTN_5	    0x00FF02FD // "5" button
+#define IR_BTN_6	    0x00FFC23D // "6" button
+#define IR_BTN_7	    0x00FFE01F // "7" button
+#define IR_BTN_8	    0x00FFA857 // "8" button
+#define IR_BTN_9	    0x00FF906F // "9" button
+#define IR_BTN_0	    0x00FF9867 // "10" button
+
+/* Other Sensor Pins */
+#define ECHO_PIN          4 // Ultrasonic sensor echo pin     -> pin D4
+#define TRIG_PIN          5 // Ultrasonic sensor trigger pin  -> pin D5
+#define RIGHT_IR_PAIR_PIN 2 // IR sensor pair yellow wire     -> pin D2
+#define LEFT_IR_PAIR_PIN  3 // IR sensor pair green wire      -> pin D3
+#define BUZZER_PIN        A0
 
 /* Constants */
 #define HALF_SPD_OF_SOUND  0.01715
 #define MOVE_SPD           90
 #define TURN_SPD           170
+
+/* Other Variables */
+bool lockFlag = LOW;
+int leftIRPairValue = 0;
+int rightIRPairValue = 0;
+bool irPairsFollowObjectFlag = LOW;
 
 /* Movement Functions */
 void moveForward(){
@@ -107,40 +116,105 @@ void moveSetSpd(int left_spd,int right_spd){
 }
 
 /* IR Button-Command Logic */
-void irMoveCMD(int received){
+bool irMoveCMD(int received){
   switch(received){
     case IR_BTN_UP:{
-      moveForward();
+      if(lockFlag == LOW){
+        moveForward();
+      }
       break;
     }
     case IR_BTN_DOWN:{
-      moveBack();
+      if(lockFlag == LOW){
+        moveBack();
+      }
       break;
     }
     case IR_BTN_RIGHT:{
-      moveRight();
+      if(lockFlag == LOW){
+        moveRight();
+      }
       break;
     }
     case IR_BTN_LEFT:{
-      moveLeft();
+      if(lockFlag == LOW){
+        moveLeft();
+      }
       break;
     }
     case IR_BTN_OK:{
-      moveStop();
+      if(lockFlag == LOW){
+        moveStop();
+      }
       break;
     }
     case IR_BTN_STAR:{
-      moveBackLeft();
+      lockFlag = HIGH;
+      enableBuzzer();
+      return lockFlag;
       break;
     }
     case IR_BTN_HASH:{
-      moveBackRight();
+      lockFlag = LOW;
+      enableBuzzer();
+      delay(200);
+      enableBuzzer();
+      return lockFlag;
       break;
     }
+    case IR_BTN_0:{
+      if(lockFlag == LOW){
+        irPairsFollowObjectFlag = !irPairsFollowObjectFlag;
+        return irPairsFollowObjectFlag;
+      }
+      break;
+    }
+    /*
+    case IR_BTN_1:{
+      break;
+    }
+    case IR_BTN_2:{
+      break;
+    }
+    case IR_BTN_3:{
+      break;
+    }
+    case IR_BTN_4:{
+      break;
+    }
+    case IR_BTN_5:{
+      break;
+    }
+    case IR_BTN_6:{
+      break;
+    }
+    case IR_BTN_7:{
+      break;
+    }
+    case IR_BTN_8:{
+      break;
+    }
+    case IR_BTN_9:{
+      break;
+    }
+    */
     default:{
       break;
     }
   }
+}
+
+void enableBuzzer(){
+  for(int b = 0; b < 10; b++){
+   digitalWrite(BUZZER_PIN, LOW);
+   delay(2);
+   digitalWrite(BUZZER_PIN, HIGH);
+   delay(2);
+  }
+}
+
+void disableBuzzer(){
+  digitalWrite(BUZZER_PIN, HIGH);
 }
 
 /* Sensor Logic */
@@ -162,7 +236,61 @@ long getUltrasonicDistance(){
   return distance;
 }
 
+void ultrasonicAvoidObject(long distance){
+  if((distance < 10) && (lockFlag == LOW)){
+    enableBuzzer();
+    moveStop();
+    moveBack();
+    delay(250);
+    moveStop();
+  }
+}
+
+void irPairsFollowObject(int leftIRPairValue, int rightIRPairValue){
+  if((leftIRPairValue == LOW) && (rightIRPairValue == LOW)){ 
+    moveBack();
+  }
+  else if((leftIRPairValue == HIGH) && (rightIRPairValue == HIGH)){
+    moveStop();
+  }
+  else if((leftIRPairValue == LOW) && (rightIRPairValue == HIGH)){ 
+    moveBackLeft();
+  }
+  else if((leftIRPairValue == HIGH) && (rightIRPairValue == LOW)){ 
+    moveBackRight();
+  }
+}
+
+void irPairsAvoidObject(int leftIRPairValue, int rightIRPairValue){
+  if((leftIRPairValue == LOW) && (rightIRPairValue == LOW) && (irPairsFollowObjectFlag == LOW) && (lockFlag == LOW)){
+   enableBuzzer();
+   moveStop();
+   moveForward();
+   delay(250);
+   moveStop();
+  }
+  else if((leftIRPairValue == LOW) && (rightIRPairValue == HIGH) && (irPairsFollowObjectFlag == LOW) && (lockFlag == LOW)){
+   enableBuzzer();
+   moveStop();
+   moveForward();
+   delay(250);
+   moveStop();
+  }
+  else if((leftIRPairValue == HIGH) && (rightIRPairValue == LOW) && (irPairsFollowObjectFlag == LOW) && (lockFlag == LOW)){
+   enableBuzzer();
+   moveStop();
+   moveForward();
+   delay(250);
+   moveStop();
+  }
+}
+
 void setup(){
+
+  /* Stop Buzzing ASAP */
+  pinMode(BUZZER_PIN, OUTPUT);
+  disableBuzzer();
+  
   /* Start the receiver */
   irrecv.enableIRIn();
   
@@ -175,15 +303,27 @@ void setup(){
   pinMode(LEFT_SPD_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIG_PIN, OUTPUT);
-  //pinMode(IR_GRN_PIN, TBD);
-  //pinMode(IR_YLW_PIN, TBD);
+  pinMode(RIGHT_IR_PAIR_PIN, INPUT);
+  pinMode(LEFT_IR_PAIR_PIN, INPUT);
 }
 
 void loop(){
+  long distance = getUltrasonicDistance();
+  rightIRPairValue = digitalRead(RIGHT_IR_PAIR_PIN);
+  leftIRPairValue = digitalRead(LEFT_IR_PAIR_PIN);
+
+  if((irPairsFollowObjectFlag == HIGH) && (lockFlag == LOW)){
+    irPairsFollowObject(leftIRPairValue, rightIRPairValue);
+  }
+
+  irPairsAvoidObject(leftIRPairValue, rightIRPairValue);
+
+  ultrasonicAvoidObject(distance);
+
   if(irrecv.decode(&IRresults)){     // Check if an IR signal has been received
     int received = IRresults.value;  // Get the received IR command
     irrecv.resume();                 // Continue receiving IR commands
     irMoveCMD(received);             // Move the robot received IR command
-  }
   delay(20);  // Wait for 20 ms
+  }
 }
